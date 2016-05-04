5fe5876c5ba40cef9f48ecf7342dbd2da247888b
tommypacker <tommypacker@gmail.com>
2016-05-04 01:21:22 -0500

    jobsboardapp

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ pFlag = False
 
 @app.route('/')
 def home():
-	return render_template('home.html', flag=pFlag)
+	if(gamesJSON != {}):
+		return render_template('home.html', flag=pFlag, testData=json.dumps(gamesJSON))
+	else:
+		return render_template('home.html', flag=pFlag, testData="")
 
 @app.route("/analyze/", methods=['POST'])
 def crunchProfile():
@@ -34,16 +37,16 @@ def recommend_stuff(profile_id): #gets the json result from recommend.py
 		games.append(rec_dict)
 	global gamesJSON
 	gamesJSON = ({"games": games}) #json of recommendations
-	writeJSONToDirectory()
+	writeJSONToDirectory(gamesJSON)
 
-def writeJSONToDirectory():
+def writeJSONToDirectory(jsonArg): #just for reference
 	fd = open('static/data.json', 'w')
-	data = json.dumps(gamesJSON)
+	data = json.dumps(jsonArg)
 	fd.write(data)
 	fd.close()
 
 def checkSteamURL(url):
-	return "https://steamcommunity.com/id/" in url or "http://steamcommunity.com/id/" in url
+	return "https://steamcommunity.com/id/" in url or "http://steamcommunity.com/id/" in url or "http://steamcommunity.com/profiles/" in url or "https://steamcommunity.com/profiles/" in url
 
 if __name__ == "__main__":
 	app.debug = True
--- static/loadData.js
+++ static/loadData.js
@@ -1,22 +1,17 @@
 var length = 0;
 
-function loadJSON(){
+function loadJSON(data){
     $(document).ready(function()
     {
-        $.getJSON("/static/data.json", function(data) {
-            console.log("success");
-        })
-        .done(function(data){
-            console.log(data.games[0]);
-            length = data.games.length;
-            for(i = 0; i<length; i++){
-                writeToDom(data.games[i].title, data.games[i].fromTitle, i);
-            }
-        });
+        console.log(data.games);
+        length = data.games.length;
+        for(i = 0; i<length; i++){
+            writeToDom(data.games[i].title, data.games[i].fromTitle, i);
+        }
     });
 }
 
 function writeToDom(recTitle, fromTitle, index){
-     $("#results").append("<div><h4>Game# " + (i+1) + "</h4>" + "<div> Recommended: " + recTitle + "</div><div> From: " + fromTitle + "</div></div>");
-     $("#results").append("<br>")
+    $("#results").append("<div><h4>Game# " + (i+1) + "</h4>" + "<div> Recommended: " + recTitle + "</div><div> From: " + fromTitle + "</div></div>");
+    $("#results").append("<br>")
 }
